cd238a8ba77a5c33eed0ab339a534fedf50d3f95
Dmitri <detail@detail.(none)>
2009-10-23 23:49:59 +0400

    minor changes

--- argsdict.py
+++ argsdict.py
@@ -2,7 +2,7 @@
 import sys, re
 """Command line parameters extracter"""
 
-def getParamsDict(*args):
+def args(*args):
 	"""arguments: a string of space-separated parameters, a list, or a number of arguments.
 	Returns a dict of parameter names and values.
 	Example: a script called with parameters: --host=lor_sam_pau --user=fei --pass=stars\ are\ bright\ tonight --db=pirate_bay
@@ -11,9 +11,9 @@ def getParamsDict(*args):
 	if len(args) == 1:
 		if isinstance(args[0], str):
 			args = re.split('\s+', args[0].strip()) # if it's one string, let's split it by spaces
-	
+		elif hasattr(args[0], '__iter__'):
+			args = args[0]
 	pars = dict((p, '--'+p+'=') for p in map(str, args)) # to use pairs: (v, --v=) for convenience.
-	
 	return dict((p, a[len(pars[p]):]) for p in pars for a in sys.argv[1:] if a[0:len(pars[p])] == pars[p])
 
 
--- vhost.py
+++ vhost.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import os, sys, re
-
+import os, sys, re, argsdict
+args = {'admin': 'webmaster@localhost', 'override-docroot': 'All', 'override-cgi': 'All', 'host-template': 'config.template'}
+add_args = ['host', 'docroot']
 if __name__ == '__main__':
 	if '--help' in sys.argv:
 		print """
@@ -9,18 +10,21 @@ if __name__ == '__main__':
 		Usage: vhost --host=<hostname> [--docroot=<document root>]
 
 		Parameters:
-			--host		Apache virtual host name
-			--docroot	Apache document root.
-
+			--host			Apache virtual host name
+			--docroot		Apache document root.
+			--admin			Admin's contact email
+			--override-docroot	AllowOverride for DocumentRoot
+			--override-cgi		AllowOverride for cgi-bin
+			--host-template		Template for host configuration (./config.template by default)
 		"""
 		sys.exit()
 	
-	# create apache vhost file
 	# link it
 	# create docroot
 	# add files templates
 	# add to /etc/hosts
 	# restart apache
+	args.update(argsdict.args(args.keys() + add_args))
 	
 	serv = 'apache2'
 	with os.popen('whereis ' + serv) as response:
@@ -30,4 +34,19 @@ if __name__ == '__main__':
 		print "No apache config found"
 		sys.exit()
 	
-	print where
+	# create apache vhost file
+	try:
+		with open(args['host-template']) as conf_file:
+			vhost_cfg = ''.join(conf_file).format(**args)
+	except IOError:
+		print 'Can\'t open file \'{0}\'. Error #{1[0]}: {1[1]}.'.format(args['host-template'], sys.exc_info()[1].args)
+		if sys.exc_info()[1][0] == 13:
+			print 'Check file access permissions.'
+		
+		sys.exit()
+	except KeyError:
+		print '\nOops, your template \'{0}\' has placeholders for parameters\nthat were not supplied in the command line:\n - {1}\n\nCan\'t proceed. Ending. Nothing has been changed yet.'.format(args['host-template'], '\n - '.join(sys.exc_info()[1].args))
+		sys.exit()
+	
+	print vhost_cfg
+	
