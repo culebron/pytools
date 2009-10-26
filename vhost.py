0d2a053ac94c6201a45101fe050921b005857a64
Dmitri 'detail' Lebedev <ryba4@ryba4.com>
2009-10-26 13:24:25 +0300

    vhost.py now works with optparse module

--- vhost.py
+++ vhost.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
-import os, sys, re, argsdict
+import os, sys, re
 
 from optparse import OptionParser
-parser = OptionParser()
+parser = OptionParser(usage='%prog SERVERNAME [options]')
 
 
-argstuple = (('server', 'Apache virtual host name'), ('docroot', 'Apache document root.'), ('admin','Admin\'s contact email', 'webmaster@localhost'), ('override-docroot', 'AllowOverride for DocumentRoot', 'All'), ('override-cgi', 'AllowOverride for cgi-bin', 'All'), ('host-template', 'Template for host configuration (./config.template by default)', 'config.template'))
+argstuple = ({'name': 'server', 'help': 'Apache virtual host name', 'short': 's', 'metavar': 'SERVER'}, {'name': 'docroot', 'help': 'Apache document root.', 'short': 'd', 'default': '{0}'}, {'name': 'admin', 'help': 'Admin\'s contact email', 'default': 'webmaster@localhost', 'short': 'a'}, {'name': 'override_docroot', 'help': 'AllowOverride for DocumentRoot', 'default': 'All'}, {'name': 'override_cgi', 'help': 'AllowOverride for cgi-bin', 'default': 'All'}, {'name': 'host_template', 'help': 'Template for host configuration (./config.template by default)', 'default': 'config.template'})
 
-args = dict((i[0], i[2]) for i in argstuple if len(i) == 3)
-req_args = [i[0] for i in argstuple if len(i) < 3]
+
+"""args = dict((i[0], i[2]) for i in argstuple if len(i) == 3)
+req_args = [i[0] for i in argstuple if len(i) < 3]"""
 
 serv = 'apache2'
 
@@ -23,17 +24,24 @@ def quit(msg, status = 0):
 	sys.exit(status)
 
 if __name__ == '__main__':
-	args.update(argsdict.args(args.keys() + req_args))
+	for i in argstuple:
+		args = ['--'+i.pop('name'), 'short' in i.keys() and '-'+ i.pop('short', '') or '']
+		parser.add_option(*args, **i)
 	
-	if '--help' in sys.argv or not set(req_args).issubset(set(args.keys())): # required arguments must be supplied in CL
-		quit(help())
+	(options, arguments)  = parser.parse_args()
 	
+	if len(arguments) < 1:
+		quit(parser.format_help())
+	
+	args = options.__dict__
+	args['server'] = arguments[0]
+
 	with os.popen('whereis ' + serv) as response:
 		where = [x for x in re.split('\s+', ''.join(response)[len(serv)+1:].strip()) if '/etc/' in x]
 	
 	if len(where) == 0:
 		quit('No apache config found')
-
+	
 	# check if there's no other same named host
 	hosts = [f for i in where for f in os.listdir(i + '/sites-enabled') if f[-1] != '~']
 	for i in hosts:
@@ -43,7 +51,7 @@ if __name__ == '__main__':
 					if re.split('\s+', l.strip('\t ')) == ['ServerName', args['host']]:
 						quit()
 		except IOError:
-			
+			pass
 	
 	# need to check if docroot does not exists or is empty
 	if os.path.lexists(args['docroot']):
@@ -55,12 +63,12 @@ if __name__ == '__main__':
 	
 	# create apache vhost file
 	try:
-		with open(args['host-template']) as conf_file:
+		with open(args['host_template']) as conf_file:
 			vhost_cfg = ''.join(conf_file).format(**args)
 	except IOError:
-		quit('Can\'t open file \'{0}\'. Error #{1[0]}: {1[1]}. {2}'.format(args['host-template'], sys.exc_info()[1].args, (sys.exc_info()[1][0] == 13 and 'Check file access permissions.' or '')), 1)
+		quit('Can\'t open file \'{0}\'. Error #{1[0]}: {1[1]}. {2}'.format(args['host_template'], sys.exc_info()[1].args, (sys.exc_info()[1][0] == 13 and 'Check file access permissions.' or '')), 1)
 	except KeyError:
-		quit('\nOops, your template \'{0}\' has placeholders for parameters\nthat were not supplied in the command line:\n - {1}\n\nCan\'t proceed. Ending. Nothing has been changed yet.'.format(args['host-template'], '\n - '.join(sys.exc_info()[1].args)), 1)
+		quit('\nOops, your template \'{0}\' has placeholders for parameters\nthat were not supplied in the command line:\n - {1}\n\nCan\'t proceed. Ending. Nothing has been changed yet.'.format(args['host_template'], '\n - '.join(sys.exc_info()[1].args)), 1)
 	
 	print vhost_cfg
 
