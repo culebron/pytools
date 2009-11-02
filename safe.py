540804848d503315a71065a6e4905a1bf07321de
Dmitri <detail@detail.(none)>
2009-11-02 19:46:30 +0300

    fix: decorator didn't work

--- safe.py
+++ safe.py
@@ -1,35 +1,6 @@
 #!/usr/bin/python
 import sys
 
-def fopen(*args):
-	try:
-		return open(*args)
-	except IOError:
-		sys.exit('Error when tried to open file \'{0}\'. Error #{1[0]}: {1[1]}'.format(args[0], sys.exc_info()[1].args))
-
-@require('msg', str)
-def quit(msg, status = 0):
-	print msg
-	sys.exit(status)
-
-def catch(method, args, message):
-	return catch_err(method, args, message, (IOError, OSError))
-
-def catch_err(method, args, message, exceptions):
-	if not isinstance(args, (list, tuple)):
-		args = [args]
-	
-	if not isinstance(message, str):
-		raise TypeError('message parameter must be a string')
-	
-	try:
-		return method(*args)
-	except exceptions:
-		quit(message.format(*args) + '\nError #{0[0]}: {0[1]}'.format(sys.exc_info()[1].args), 1)
-
-def deco(method, exceptions):
-	pass
-
 def require(arg_name, *allowed_types):
 	def make_wrapper(f):
 		if hasattr(f, "wrapped_args"):
@@ -62,3 +33,34 @@ def require(arg_name, *allowed_types):
 		return wrapper
 
 	return make_wrapper
+
+def fopen(*args):
+	try:
+		return open(*args)
+	except IOError:
+		sys.exit('Error when tried to open file \'{0}\'. Error #{1[0]}: {1[1]}'.format(args[0], sys.exc_info()[1].args))
+
+@require('msg', str)
+def quit(msg, status = 0):
+	print msg
+	sys.exit(status)
+
+def catch(method, args, message):
+	return catch_err(method, args, message, (IOError, OSError))
+
+def catch_err(method, args, message, exceptions):
+	if not isinstance(args, (list, tuple)):
+		args = [args]
+	
+	if not isinstance(message, str):
+		raise TypeError('message parameter must be a string')
+	
+	try:
+		return method(*args)
+	except exceptions:
+		quit(message.format(*args) + '\nError #{0[0]}: {0[1]}'.format(sys.exc_info()[1].args), 1)
+
+def deco(method, exceptions):
+	pass
+
+
