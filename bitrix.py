5a5d69f925e367f9786f001c152a881b49d8009b
Dmitri <detail@detail.(none)>
2009-12-08 23:16:24 +0300

    fix: path to bitrix setup was wrong

--- bitrix.py
+++ bitrix.py
@@ -4,7 +4,7 @@ import os, shutil, parse, safe, apachehost
 from random import Random
 
 parse.parser.usage = '%prog HOST'
-bitrix_file = 'bitrix8setup.php'
+bitrix_file = 'templates/bitrix8setup.php'
 parse.shovel({'name': 'installer', 'help': 'Path to bitrix installer script', 'short': 'd', 'default': os.path.join(os.path.dirname(os.path.realpath(__file__)), bitrix_file)})
 
 if __name__ == '__main__':
